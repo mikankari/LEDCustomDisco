89d18094e9955b1b79d0e09692d3e0d1c97d0b3f
mikankari <doitu1995@yahoo.co.jp>
2015-08-10 16:55:15 +0900

    add build data

---
+++ app/build/generated/source/buildConfig/debug/jp/ac/kanagawa_it/s1223066/ledcustomdisco/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jp.ac.kanagawa_it.s1223066.ledcustomdisco;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "jp.ac.kanagawa_it.s1223066.ledcustomdisco";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
